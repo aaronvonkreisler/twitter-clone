d1fc452814331f0026c19a5003fbd999f6b5f39e
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-24 15:47:18 -0600

    mobile sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Sidebar from './components/sidebar/Sidebar';
+import { useMediaQuery } from '@material-ui/core';
+import SmallSidebar from './components/sidebar/SmallSidebar';
 import './App.css';
 
 const App = () => {
+   const largeDevice = useMediaQuery('(min-width:1450px)');
    return (
-      <div className="app">
-         <Sidebar />
-      </div>
+      <div className="app">{largeDevice ? <Sidebar /> : <SmallSidebar />}</div>
    );
 };
 
--- src/components/sidebar/SidebarItem.js
+++ src/components/sidebar/SidebarItem.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { IconButton } from '@material-ui/core';
 import './SidebarItems.css';
-const SidebarItem = ({ active, text, Icon }) => {
+const SidebarItem = ({ active, text, Icon, mobile }) => {
    return (
-      <div className={`sidebar-option ${active && 'sidebar-option--active'}`}>
-         <Icon />
-         <span>{text}</span>
-      </div>
+      <React.Fragment>
+         {mobile ? (
+            <div className="mobile-sidebar-option">
+               <IconButton>
+                  <Icon />
+               </IconButton>
+            </div>
+         ) : (
+            <div
+               className={`sidebar-option ${
+                  active && 'sidebar-option--active'
+               }`}
+            >
+               <Icon />
+               {text && <span>{text}</span>}
+            </div>
+         )}
+      </React.Fragment>
    );
 };
 
