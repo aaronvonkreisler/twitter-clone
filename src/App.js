ec1022dc9949de90d489815b42098da6b6ce952b
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-24 16:03:08 -0600

    sidebar util renders upon screen size

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Sidebar from './components/sidebar/Sidebar';
-import { useMediaQuery } from '@material-ui/core';
-import SmallSidebar from './components/sidebar/SmallSidebar';
+import SidebarUtil from './components/sidebar/SidebarUtil';
 import './App.css';
 
 const App = () => {
-   const largeDevice = useMediaQuery('(min-width:1450px)');
    return (
-      <div className="app">{largeDevice ? <Sidebar /> : <SmallSidebar />}</div>
+      <div className="app">
+         <SidebarUtil />
+      </div>
    );
 };
 
