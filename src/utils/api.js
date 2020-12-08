cdf0dd917d2aeaeaf46003addbe9e9a9dc280a6c
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-12-08 17:48:27 -0600

    axios interceptor to log out on 401 status

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import store from '../store/store';
+import { LOG_OUT } from '../actions/types';
 
 const api = axios.create({
    baseURL: 'https://tweeter-v1-api.herokuapp.com/',
@@ -7,5 +8,18 @@ const api = axios.create({
       'Content-Type': 'application/json',
    },
 });
+
+// Intercept any unauthorized error responses to the API
+// and check if the token has expired and log out the user
+
+api.interceptors.response.use(
+   (res) => res,
+   (err) => {
+      if (err.response.status === 401) {
+         store.dispatch({ type: LOG_OUT });
+      }
+      return Promise.reject(err);
+   }
+);
 
 export default api;
