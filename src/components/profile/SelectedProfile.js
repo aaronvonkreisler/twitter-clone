1e0e0b7ff6639369473f4c2c8046606afd9d589a
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-12-16 16:21:21 -0600

    profile top section

--- src/components/profile/SelectedProfile.js
+++ src/components/profile/SelectedProfile.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Avatar } from '@material-ui/core';
+import { FiMail } from 'react-icons/fi';
 import { getUserByUsername } from '../../actions/profile';
 import Header from '../layout/Header';
 import Spinner from '../layout/Spinner';
+import '../../styles/design/profile.css';
 
 const SelectedProfile = ({
    getUserByUsername,
    match,
    profiles: { profile, loading },
+   auth,
 }) => {
    let history = useHistory();
 
@@ -27,7 +31,21 @@ const SelectedProfile = ({
                   onIconClick={() => history.goBack()}
                   text={profile.name}
                />
-               <div className="feed" />
+               <div className="profileWrapper">
+                  <div className="coverPhoto__container">
+                     <div className="userImage__container">
+                        <Avatar src={profile.avatar} alt="User profile" />
+                     </div>
+                  </div>
+                  <div className="profileButtons__container">
+                     {!auth.loading && auth.user._id !== profile._id && (
+                        <Link to={`/messages/${profile._id}`}>
+                           <FiMail />
+                        </Link>
+                     )}
+                  </div>
+                  <div className="feed" />
+               </div>
             </React.Fragment>
          )}
       </div>
@@ -40,6 +58,7 @@ SelectedProfile.propTypes = {
 
 const mapStateToProps = (state) => ({
    profiles: state.profiles,
+   auth: state.auth,
 });
 
 export default connect(mapStateToProps, { getUserByUsername })(SelectedProfile);
--- src/components/profile/UserProfile.js
+++ src/components/profile/UserProfile.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Avatar } from '@material-ui/core';
 import Header from '../layout/Header';
 import Spinner from '../layout/Spinner';
+import '../../styles/design/profile.css';
 
 const UserProfile = ({ auth: { loading, user } }) => {
    let history = useHistory();
@@ -16,9 +18,16 @@ const UserProfile = ({ auth: { loading, user } }) => {
                <Header
                   leftIcon
                   onIconClick={() => history.goBack()}
-                  text={!loading ? user.name : 'Profile'}
+                  text={user.name}
                />
-               <div className="feed" />
+               <div className="profileWrapper">
+                  <div className="coverPhoto__container">
+                     <div className="userImage__container">
+                        <Avatar src={user.avatar} alt="User profile" />
+                     </div>
+                  </div>
+                  <div className="feed" />
+               </div>
             </React.Fragment>
          )}
       </React.Fragment>
