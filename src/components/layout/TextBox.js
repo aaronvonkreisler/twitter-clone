4f49062c03f571202a1d656ea78bbce2133834cf
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-12-26 21:32:48 -0600

    add maxlength to textbox component

--- src/components/layout/TextBox.js
+++ src/components/layout/TextBox.js
@@ -1,13 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "../../styles/design/login.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../styles/design/login.css';
+import '../../styles/design/textBox.css';
 const TextBox = ({
   value,
   onChange,
   label,
   inputName,
   inputType,
+  textArea,
   style,
+  counter,
+  count,
+  maxLength,
+  rows,
+
   ...props
 }) => {
   return (
@@ -15,17 +22,38 @@ const TextBox = ({
       <label className="label">
         <div className="label-sub1">
           <div className="label-sub2">
-            <span>{label}</span>
+            {counter ? (
+              <div className="displayCounter">
+                <div className="text">{label}</div>
+                <div className="number">
+                  {count}/{maxLength}
+                </div>
+              </div>
+            ) : (
+              <span>{label}</span>
+            )}
             <div className="label-sub3">
               <div className="label-sub4">
-                <input
-                  type={inputType}
-                  className="email-input"
-                  name={inputName}
-                  value={value}
-                  onChange={onChange}
-                  {...props}
-                />
+                {textArea ? (
+                  <textarea
+                    name={inputName}
+                    value={value}
+                    onChange={onChange}
+                    className="email-input"
+                    maxLength={maxLength}
+                    rows={rows}
+                  />
+                ) : (
+                  <input
+                    type={inputType}
+                    className="email-input"
+                    name={inputName}
+                    value={value}
+                    onChange={onChange}
+                    maxLength={maxLength}
+                    {...props}
+                  />
+                )}
               </div>
             </div>
           </div>
@@ -35,11 +63,18 @@ const TextBox = ({
   );
 };
 
+TextBox.defaultProps = {
+  counter: false,
+};
+
 TextBox.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
   inputName: PropTypes.string.isRequired,
+  counter: PropTypes.bool,
+  count: PropTypes.number,
+  textArea: PropTypes.bool,
 };
 
 export default TextBox;
