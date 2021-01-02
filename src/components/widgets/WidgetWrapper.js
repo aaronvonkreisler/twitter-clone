611da2af946af48a8d3b8d3074f75a3820911c3d
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-01-02 16:53:37 -0600

    remove unused vars

--- src/components/widgets/WidgetWrapper.js
+++ src/components/widgets/WidgetWrapper.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import PropTypes from 'prop-types';
+
 import SearchUsers from './SearchUsers';
 import SuggestedFollowCard from './SuggestedFollow';
 import RelevantPeople from './RelevantPeople';
 import '../../styles/design/widgetWrapper.css';
+
 const WidgetWrapper = (props) => {
    let location = useLocation();
    const isStatusPageRendered = location.pathname.split('/')[2] === 'status';
@@ -17,7 +18,5 @@ const WidgetWrapper = (props) => {
       </div>
    );
 };
-
-WidgetWrapper.propTypes = {};
 
 export default WidgetWrapper;
