1535bc4f0d364eed848fdf8ac57a8d952e3f557a
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-01-03 16:21:51 -0600

    sidebar more menu

--- src/components/sidebar/MenuButton.js
+++ src/components/sidebar/MenuButton.js
@@ -2,28 +2,50 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const MenuButton = ({ text, path, Icon, hideSmall, hideMedium }) => {
+const MenuButton = ({
+   text,
+   path,
+   Icon,
+   hideSmall,
+   hideMedium,
+   link,
+   ...props
+}) => {
    const small = hideSmall ? 'hide-small' : '';
    const medium = hideMedium ? 'hide-medium' : '';
    return (
       <li className={`main-nav-item ${small} ${medium}`}>
-         <NavLink
-            exact
-            to={path}
-            className="menu-button__link menu-item"
-            activeClassName="menu-button__active"
-         >
-            <span className="icon">
-               <Icon />
-            </span>
-            <span className="text">{text}</span>
-         </NavLink>
+         {link && (
+            <NavLink
+               exact
+               to={path}
+               className="menu-button__link menu-item"
+               activeClassName="menu-button__active"
+            >
+               <span className="icon">
+                  <Icon />
+               </span>
+               <span className="text">{text}</span>
+            </NavLink>
+         )}
+         {!link && (
+            <div className="menu-item" {...props}>
+               <span className="icon">
+                  <Icon />
+               </span>
+               <span className="text">{text}</span>
+            </div>
+         )}
       </li>
    );
 };
 
+MenuButton.defaultProps = {
+   link: true,
+};
 MenuButton.propTypes = {
-   path: PropTypes.string.isRequired,
+   path: PropTypes.string,
+   link: PropTypes.bool,
 };
 
 export default MenuButton;
