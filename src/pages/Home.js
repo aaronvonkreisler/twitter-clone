dfc3f1690645d3501213bdf122f21aa31bcc3b9b
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-01-11 17:59:25 -0600

    removed unuses useState import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { HiOutlineSparkles } from 'react-icons/hi';
 import Header from '../components/layout/Header';
